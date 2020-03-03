b512793fb31e03447ddb06e1c453479fd0bcb66a
floriancourgey <florian@courgey.fr>
2020-03-03 10:46:48 +0100

    + text varies

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
+var moment = require('moment');
+
 exports.index = function(req, res, next) {
-    // res.render('index', { title: 'Express' });
-    res.render('index')
+    var text = 'OUI';
+    const now = moment();
+    if(now.format('dddd') === 'Friday'){
+      text = 'OUI, vendredi tout est permis !';
+    }
+    res.render('index', {
+      text: text,
+    })
 };
