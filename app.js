286fc403cbdefb09256c903a108d8f3a19a1964d
floriancourgey <florian@courgey.fr>
2020-03-03 10:33:31 +0100

    + index controller in /routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const routes = require('./routes/index.js')
 
 express()
   .use('/public', express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
+  .get('/', routes.index)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
