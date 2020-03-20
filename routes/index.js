e549cfafa62f037be775a05eae23c54363264100
floriancourgey <florian@courgey.fr>
2020-03-20 16:48:05 +0100

    +

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,16 @@ exports.index = function(req, res, next) {
     var text = 'OUI';
     const now = moment();
     const minutes = now.minutes();
-    if(minutes === 20){
-      text = "OUI, à 20, c'est l'heure du vin !";
+    if(false){
+    } else if(minutes === 5){
+      text = "OUI, à cinque, on trinque !";  
     } else if(minutes === 15){
       text = "OUI, à et quart, Ricard !";
-    }else if(minutes === 45){
+    } else if(minutes === 20){
+      text = "OUI, à 20, c'est l'heure du vin !";
+    } else if( minutes === 45){
       text = "OUI, moins l'quart, Ricard !";
-    }else if(now.format('dddd') === 'Friday'){
+    } else if(now.format('dddd') === 'Friday'){
       text = 'OUI, vendredi tout est permis !';
     }
     res.render('index', {
