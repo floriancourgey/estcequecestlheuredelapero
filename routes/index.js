2e8761f5c3371da768289dc7320fa6ac0637e09d
floriancourgey <florian@courgey.fr>
2020-03-20 16:45:59 +0100

    go

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var moment = require('moment');
 exports.index = function(req, res, next) {
     var text = 'OUI';
     const now = moment();
-    if(now.format('dddd') === 'Friday'){
+    const minutes = now.minutes();
+    if(minutes === 20){
+      text = "OUI, à 20, c'est l'heure du vin !";
+    } else if(minutes === 15){
+      text = "OUI, à et quart, Ricard !";
+    }else if(minutes === 45){
+      text = "OUI, moins l'quart, Ricard !";
+    }else if(now.format('dddd') === 'Friday'){
       text = 'OUI, vendredi tout est permis !';
     }
     res.render('index', {
